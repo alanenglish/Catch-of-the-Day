40a523e96fd697c20e9ebd2768fff844fc7a0924
Alan English <aenglish@manorhillcapital.com>
2017-10-04 12:35:41 -0500

    add onSubmit function to form, which calls function goToStore and is bound to this, add default value and ref to input tag, ref allows us to grab input to use

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <h1>NotFound</h1>
+      <h1>URL Not Found...</h1>
     );
   }
 }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  // constructor is code that runs when component is created
+  // constructor() {
+  //   super();
+  //   // looks for goToStore method, sets itself to its own self and binds to this
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  // other way to do above is to bind goToStore to this in the onSubmit function
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log('you changed the url');
+    // first grab the text from the box
+    // this is null in own created method - doesnt implicitly bind all thhe methods to this
+    console.log(this.storeInput.value);
+    // second we're going to transition from / to /store/storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      // <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+      // another option above is same as below form tag
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     );
