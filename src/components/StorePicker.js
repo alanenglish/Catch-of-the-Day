f0b3adc932aeaa28e276cf56faa99aa30d2b97a8
Alan English <aenglish@manorhillcapital.com>
2017-10-04 11:43:15 -0500

    create Order, Header and Inventory class components with boilerplate, refactor index file to import App and render App

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class StorePicker extends Component {
   render() {
     return (
-      <p>hello</p>
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
     );
   }
 }
