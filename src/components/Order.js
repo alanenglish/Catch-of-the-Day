586b7551fc444947d8ae7530133e1a54af57894a
Alan English <aenglish@manorhillcapital.com>
2017-10-05 12:25:54 -0500

    add styling and order animations to order form item addition and subtraction using CSS Transition Group

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends Component {
   constructor() {
@@ -39,13 +40,19 @@ class Order extends Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     );
   }
